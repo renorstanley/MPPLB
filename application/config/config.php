d7afacba21f7f9c4df4e07422f954d9d185f76f1
Andre Setiawan <Andre Setiawan>
2016-03-20 22:18:20 +0700

    rubah dikit2

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost:49152/Job3/';
+$config['base_url']	= 'http://localhost/mppl/';
 
 /*
 |--------------------------------------------------------------------------
